f2b840f20fe1b9d720fbefe96f0fd21bec34b505
altair-f <altair-f@narod.ru>
2015-12-30 23:10:56 +0600

    first inform586 commit on github, ucoz must die!

---
+++ design/infogra.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+
+
+	$('#p1').click(function(){$('#d1').show();});
+	$('#p2').click(function(){$('#d2').show();});
+	$('#p3').click(function(){$('#d3').show();});
+	$('#p4').click(function(){$('#d4').show();});
+	$('#p5').click(function(){$('#d5').show();});
+	$('#p6').click(function(){$('#d6').show();});
+	$('#p7').click(function(){$('#d7').show();});
+	$('#p8').click(function(){$('#d8').show();});
+	$('#p9').click(function(){$('#d9').show();});
+	$('#p10').click(function(){$('#d10').show();});
+	$('#p11').click(function(){$('#d11').show();});
+	$('#p12').click(function(){$('#d12').show();});
+
+
+	$('.poly div').click(function(){$('.poly div').hide();});
+
+});
